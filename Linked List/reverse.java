b9fe51e5796cd252be37653ce3ab748439c0e72c
Anlin Abishek J <165697246+Anlin-Abishek@users.noreply.github.com>
2025-09-16 08:44:51 +0530

    Create reverse.java

---
+++ Linked List/reverse.java
@@ -0,0 +1,57 @@
+class node{
+    int data;
+    node next;
+    node(int d){
+        data=d;
+        next=null;
+    }
+}
+class linkedlist{
+    node head=null;
+    node old=null;
+    void insert(int data){
+        node n=new node(data);
+        if(head==null){
+            head=n;
+            old=n;
+        }
+    else{
+        old.next=n;
+        old=n;
+        
+    }
+    }
+    
+    void display(){
+        node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+      System.out.println(slow.data);
+    }
+  void reverse(){
+        node safety=null;
+        node curr=head;
+        node prev=null;
+        while(curr!=null){
+            safety=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=safety;    }   
+        head=prev;
+}
+}
+public class Main {
+    public static void main(String[] args) {
+       Scanner z=new Scanner(System.in);
+        linkedlist lst=new linkedlist();
+        while(z.hasNextInt()){
+            int n=z.nextInt();
+            lst.insert(n);
+        }
+        lst.display();
+        lst.reverse();
+      lst.display();
+    }
+}
