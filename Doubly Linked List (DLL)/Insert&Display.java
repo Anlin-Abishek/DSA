1d69cb8db9837ac7296a267fc6e65a06b4d3809a
Anlin Abishek J <165697246+Anlin-Abishek@users.noreply.github.com>
2025-09-17 09:26:02 +0530

    Create Insert&Display.java

---
+++ Doubly Linked List (DLL)/Insert&Display.java
@@ -0,0 +1,27 @@
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int d) {
+        data = d;
+        next = null;
+        prev = null;
+    }
+}
+
+class DLL {
+    Node head = null;
+    Node tail = null;
+
+    void insert(int data) {
+        Node nn = new Node(data);
+        if (head == null) {
+            head = nn;
+            tail = nn;
+        } else {
+            tail.next = nn;
+            nn.prev = tail;
+            tail = nn;
+        }
+    }
