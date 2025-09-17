0e6da2e4bffab0b0c58631e739a794df7f27d510
Anlin Abishek J <165697246+Anlin-Abishek@users.noreply.github.com>
2025-09-17 09:41:36 +0530

    Update Insert&Display.java

--- Doubly Linked List (DLL)/Insert&Display.java
+++ Doubly Linked List (DLL)/Insert&Display.java
@@ -25,3 +25,11 @@ class DLL {
             tail = nn;
         }
     }
+ void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
